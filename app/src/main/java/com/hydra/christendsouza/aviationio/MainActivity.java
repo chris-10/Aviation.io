41e4394fdeb8b5a002751490b6fcc65c7e3abfea
chris-10 <dsouzachristen@yahoo.co.in>
2017-10-07 17:16:18 +0530

    Commit 1

--- app/src/main/java/com/hydra/christendsouza/aviationio/MainActivity.java
+++ app/src/main/java/com/hydra/christendsouza/aviationio/MainActivity.java
@@ -3,11 +3,28 @@ package com.hydra.christendsouza.aviationio;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.net.InetAddress;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        if(isInternetAvailable()) {
+            setContentView(R.layout.activity_login);
+        }
+        else{
+            setContentView(R.layout.guest_profile);
+        }
     }
+    public boolean isInternetAvailable() {
+        try {
+            InetAddress ipAddr = InetAddress.getByName("google.com"); //You can replace it with your name
+            return !ipAddr.equals("");
+        } catch (Exception e) {
+            return false;
+        }
+
+    }
+
 }
